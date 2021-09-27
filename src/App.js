910b567b46bdb6d243fd8414231897e120b3632e
mu99y <murugan.learning@gmail.com>
2021-09-27 05:53:01 +0100

    Bringing the react router to link the pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
-return (
-  <>
-    Welcome to spaceX
-  </>
-);
-}
+import { Switch, Route } from "react-router-dom";
+import UpComing from "./components/UpComing";
+import Home from "./components/Home";
+import Nav from "./Nav";
+
+const App = () => {
+  return (
+    <>
+      <div>
+       <Nav />
+        <Switch>
+          <Route exact path="/upcoming" render={() => <Home />} />
+          <Route exact path="/" render={() => <UpComing />} />
+        </Switch>
+      </div>
+    </>
+  );
+};
 
 export default App;
---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+function Nav() {
+    return (
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/upcoming">Next launch</Link>
+            </li>
+          </ul>
+        </nav>
+    )
+}
+
+export default Nav
