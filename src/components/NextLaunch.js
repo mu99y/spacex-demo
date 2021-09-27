275eecabf8d2bf1a3b7fe049cfe41a954b700e90
mu99y <murugan.learning@gmail.com>
2021-09-27 07:31:35 +0100

    Move counter to its own component

--- src/components/NextLaunch.js
+++ src/components/NextLaunch.js
@@ -1,35 +1,10 @@
-import TableContainer from "@mui/material/TableContainer";
-import Table from "@mui/material/Table";
-import TableRow from "@mui/material/TableRow";
-import TableCell from "@mui/material/TableCell";
-import { TableBody } from "@mui/material";
+import Counter from "./Counter";
 
 const NextLaunch = () => {
   return (
     <div>
       Next Launch : <span>Starlink</span>
-      <TableContainer>
-        <Table sx={{ maxWidth: 100 }}>
-          <TableBody>
-            <TableRow>
-              <TableCell>Days</TableCell>
-              <TableCell>12</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Hours</TableCell>
-              <TableCell>08</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Minutes</TableCell>
-              <TableCell>25</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Seconds</TableCell>
-              <TableCell>35</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Counter />
     </div>
   );
 };
